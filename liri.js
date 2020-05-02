74c12a8b4b74f83c06f557ae38ac49358bd50e89
Face70 <60823541+DarkKnight70@users.noreply.github.com>
2020-05-01 21:29:28 -0600

    got concert, spotify and movie all working

--- liri.js
+++ liri.js
@@ -5,86 +5,65 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var moment = require('moment');
 var axios = require('axios').default;
+var fs = require("fs");
+var inquirer = require("inquirer");
 var input = process.argv[2];
+var userOutput = process.argv[3];
 //make it so liri.js can take in one of the following commands:
 //concert-this
 if (input === "concert-this") {
-    var artist = process.argv[3]
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-        function(response){
-            console.log(response.artist.name)
-        }
-    )
-}
+    if (!userOutput){
+        userOutput = "barenaked ladies"
+    }
+    axios
+    .get("https://rest.bandsintown.com/artists/" + userOutput + "/events?app_id=codingbootcamp")
+    .then(function(response) {
+        console.log(response.data[0].lineup[0]);
+    
+    for (var i = 0; i < response.data.length; i++) {
+        
+        var result = "Venue Name: " + response.data[i].venue.name + "\n"
+                    + "City: " + response.data[i].venue.city + "\n"
+                    + "Date(MM/DD/YYYY): " + moment(response.data[i].datetime).format('MM DD YYYY') + "\n"
+        
+            
+        console.log(result);
+    }
+    });           
+};
+
 
 //spotify-this-song
 if (input === "spotify-this-song") {
-    var songName = process.argv[3];
-    if (!songName){
-        songName= "The Sign"
-    }
-    spotify.search({type: 'track', query: songName}, function(err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        } else {
-            output =
-                "|+-+-+-+-+-+->LIRI FOUND THIS FOR YOU<+-+-+-+-+-+-|" +
-                "\n" + "Artist Name: " + data.tracks.items[0].album.artists[0].name +
-                "\n" + "Song Name: " + "'" + songName.toUpperCase() + "'" +
-                "\n" + "Album Name: " + data.tracks.items[0].album.name +
-                "\n" + "URL: " + data.tracks.items[0].album.external_urls.spotify;
-            console.log(output);
-        }
-
     
+    if (!userOutput){
+        userOutput= "Black Betty"
+    }
+    spotify.search({type: 'track', query: userOutput, limit: 3 }).then(function(response) {
+       for (var i = 0; i < 3; i++) {
+           var result =
+                "\n|+-+-+-+-+-+->LIRI FOUND THIS FOR YOU<+-+-+-+-+-+-|" +
+                "\n" + "Artist/Band: " + response.tracks.items[i].album.artists[0].name +
+                "\n" + "Song: " + "'" + userOutput.toUpperCase() + "'" +
+                "\n" + "Album: " + response.tracks.items[i].album.name +
+                "\n" + "URL: " + response.tracks.items[i].album.external_urls.spotify + "\n";
+                
+                console.log(result); 
+        };
     });
 };
 
 //movie-this
 if (input === "movie-this"){
-    var movie = process.argv[3]
-    if(!movie){
-        axios.get("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy").then(
+    
+    if(!userOutput){
+        userOutput = "Logan Lucky";
+        }
+    axios.get("http://www.omdbapi.com/?t=" + userOutput + "&y=&plot=short&apikey=trilogy").then(
         function(response) {
           
           var output =
-            "|+-+-+-+-+-+->LIRI FOUND THIS FOR YOU<+-+-+-+-+-+-|" +
-                "\n" + 'Title: ' + response.data.Title +
-                "\n" + 'Year: ' + response.data.Year +
-                "\n" + 'Rated: ' + response.data.Rated +
-                "\n" + 'IMDB Rating: ' + response.data.imdbRating +
-                "\n" + 'Country: ' + response.data.Country +
-                "\n" + 'Language: ' + response.data.Language +
-                "\n" + 'Plot: ' + response.data.Plot +
-                "\n" + 'Actors: ' + response.data.Actors +
-                "\n" + 'Tomato Rating: ' + response.data.Ratings[1].Value +
-                "\n" + 'IMDb Rating: ' + response.data.imdbRating + "\n";
-
-          console.log(output);
-        })
-        .catch(function(error) {
-          if (error.response) {
-            
-            console.log("---------------Data---------------");
-            console.log(error.response.data);
-            console.log("---------------Status---------------");
-            console.log(error.response.status);
-            console.log("---------------Status---------------");
-            console.log(error.response.headers);
-          } else if (error.request) {
-        
-            console.log(error.request);
-          } else {
-            console.log("Error", error.message);
-          }
-          console.log(error.config);
-        });
-    } else {
-        axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
-        function(response) {
-          
-          var output =
-            "|+-+-+-+-+-+->LIRI FOUND THIS FOR YOU<+-+-+-+-+-+-|" +
+            "\n|+-+-+-+-+-+->LIRI FOUND THIS FOR YOU<+-+-+-+-+-+-|" +
                 "\n" + 'Title: ' + response.data.Title +
                 "\n" + 'Year: ' + response.data.Year +
                 "\n" + 'Rated: ' + response.data.Rated +
@@ -115,7 +94,6 @@ if (input === "movie-this"){
           }
           console.log(error.config);
         });
-    }
-}
+    };
 
 //do-what-it-says
